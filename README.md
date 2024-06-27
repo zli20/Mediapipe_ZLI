59c0aa772c055ef4d8a4c379929587ba59f142d1
zli2 <zli2_16@163.com>
2024-06-27 18:40:04 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Mediapipe 
+
+## python
+安装：
+```
+pip install mediapipe
+``` 
+使用： poseLandmark.py
+
+## windows c++编译
+TODO
---
+++ python/poseLandmark.py
@@ -0,0 +1,34 @@
+import mediapipe as mp
+import cv2
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils  # 画图
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_holistic = mp.solutions.holistic  # 全身识别跟踪
+
+mp_pose = mp.solutions.pose  # 姿态估计
+
+cap = cv2.VideoCapture(1)
+
+holistic = mp_holistic.Holistic(static_image_mode=False,
+                                smooth_landmarks=True,
+                                min_detection_confidence=0.5,
+                                min_tracking_confidence=0.5)
+
+pose = mp_pose.Pose(static_image_mode=False,
+                    smooth_landmarks=True,
+                    min_detection_confidence=0.5,
+                    min_tracking_confidence=0.5)
+
+while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+        print("Ignoring empty camera frame.")
+        continue
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # results = holistic.process(image_rgb)
+    results = pose.process(image_rgb)
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+    cv2.imshow("frame", image)
+    cv2.waitKey(1)
